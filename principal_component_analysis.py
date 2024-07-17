af6fd3c0df2283cfb7cfc538584374f936efc010
Christian Hardy <ChristianTH.to@gmail.com>
2024-07-17 04:33:58 -0400

    Update principal_component_analysis.py

--- principal_component_analysis.py
+++ principal_component_analysis.py
@@ -1,23 +1,24 @@
-# Needs refactoring
 # Dependencies
 import timeit
 
 import numpy as np
 import pandas as pd
 
+from load_data import load_mnist
+
+from ipywidgets import interact
+
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
-from ipywidgets import interact
+%matplotlib inline
 
-from load_data import load_mnist
 
-# Image data
+# Load MNIST dataset
 MNIST = load_mnist()
 images, labels = MNIST['data'], MNIST['target']
 
-%matplotlib inline
 
 # Quick plot of an image from the dataset to make sure data loaded properly
 plt.figure(figsize = (4,4))
@@ -25,99 +26,180 @@ plt.imshow(images[0].reshape(28,28), cmap = 'gray');
 
 
 '''
-   The next four functions preprocess the data so the images can have a zero mean and variance of one
-   The pixel encodings are originally uint8 and must be converted to a floating point number between 0-1
-   The mean μ must be subtracted from each image
-   Each image must be scaled dimensionally by 1/σ, where σ is the standard deviation
-   Xbar will be the normalized dataset
+The next four functions preprocess the data so the images can have a zero mean and 
+variance of one. The pixel encodings are originally uint8 and must be converted to 
+a floating point number between 0-1. The mean mu, μ, must be subtracted from each image.
+Each image must be scaled dimensionally by 1/σ, where σ is the standard deviation,
+X_normalized will be the normalized dataset.
 '''
+
 def normalize(X):
     '''
-        In the future, you could encounter dimensions where the standard deviation is
-        zero, for those when you do normalization the normalized data
-        will be NaN. Handle this by setting using `std = 1` for those 
-        dimensions when doing normalization.
+    Normalize the dataset X.
+
+    Parameters:
+    X: ndarray of shape (N, D)
+
+    Returns:
+    X_normalized: ndarray of shape (N, D) - normalized dataset
+    mean: ndarray of shape (D,) - mean of each feature
+    std_dev: ndarray of shape (D,) - standard deviation of each feature
+    
+     In the future, you could encounter dimensions where the standard deviation is
+     zero, for those when you do normalization the normalized data will be NaN. 
+     Handle this by setting using `std = 1` for those dimensions when doing normalization.
     '''
+    # Calculate the mean of each feature
     mu = np.mean(X, axis = 0) 
-    std = np.std(X, axis = )
-    std_filled = std.copy()
-    std_filled[std == 0] = 1.
-    Xbar = (X - mu) / std_filled                  
-    return Xbar, mu, std
+    # Calculate standard deviation of each feature
+    std_dev = np.std(X, axis = 0)
+    # Create copy of std_dev
+    std_dev_filled = std_dev.copy()
+    # Avoid division by zero by setting std_dev to 1 where it is 0
+    std_dev_filled[std == 0] = 1.0
+    # Normalize the dataset
+    X_normalized = (X - mu) / std_dev_filled                  
+    return X_normalized, mu, std_dev
 
 
-# Computes the eigenvalues and corresponding eigenvectors for the covariance matrix S and sorts by the 
-# largest eigenvalues and corresponding eigenvectors
-def eig(S):
+def compute_eigen(S):
     ''' 
-        the eigenvals and eigenvecs should be sorted in descending
-        order of the eigen values
+    Parameters:
+    S: ndarray of shape (D, D)
+
+    Returns:
+    eigen_values: ndarray of shape (D,) - sorted eigenavlues
+    eigen_vectors: ndarray of shape (D, D) - corresponding eigenvectors
+     
+    Computes the eigenvalues and corresponding eigenvectors for the covariance matrix S 
+    and sorts them in descending order of eigenvalues.
     '''
+    # Compute eigenvalues and eigenvectors
     eigen_values, eigen_vectors = np.linalg.eig(S)
-    idx = eigen_values.argsort()[:: - 1]
-    eigen_values = eigen_values[idx]
-    eigen_vectors = eigen_vectors[:, idx]
+    # Get indices of sorted eigenvalues in descending order
+    sorted_indices = eigen_values.argsort()[:: - 1]
+    # Sort eigenvalues
+    eigen_values = eigen_values[sorted_indices]
+    # Sort eigenvectors
+    eigen_vectors = eigen_vectors[:, sorted_indices]
     return (eigen_values, eigen_vectors) 
 
 
-# Computes the projection matrix onto the space spanned by `B` where:
-# B = ndarray of dimension (D, M), the basis for the subspace and returns P = projection matrix
 def projection_matrix(B):
+   '''
+   Parameters:
+   B = ndarray of shape (D, M) is the basis for the subspace 
+   
+   Returns:
+   P = projection matrix of shape (D, D)
+   
+   Computes the projection matrix onto the space spanned by B.
+   '''
+   # Compute projection matrix
     return B @ (np.linalg.inv(B.T @ B)) @ B.T
  
 
-'''
-   X = ndarray of size (N, D), where D is the dimension of the data,
-   and N is the number of datapoints
-   num_components = the number of principal components to use
-   X_reconstruct: ndarray of the reconstruction of X from the first `num_components` principal components
-'''
-def PCA(X, num_components):
-    X, mean, std = normalize(X)
-    S = np.cov(X, rowvar = False, bias = True)
-    eig_vals, eig_vecs = eig(S)
-    P = projection_matrix(eig_vecs[:, :num_components])
-    X_reconstruct = (P @ X.T).T
+def pca(X, num_components):
+   '''
+   Parameters:
+   X: ndarray of size (N, D), where N is the number of datapoints and D is the 
+   dimension of the data.
+   
+   num_components: int - number of principal components to use
+
+   Returns:
+   X_reconstructed: ndarray of shape (N, D) the reconstruction of X from the 
+   first `num_components` principal components
+
+   Perform principal component analysis (PCA) on dataset X.
+   '''
+    # Normalize the dataset
+    X_normalized, mu, std_dev = normalize(X)
+    # Compute covariance matrix
+    eigen_values, eigen_vectors = np.cov(X_normalized, rowvar = False, bias = True)
+    # Compute eigenvalues and eigenvectors
+    eigen_values, eigen_vectors = compute_eigens(covariance_matrix)
+    # Compute projection matrix
+    projection_matrix = compute_projection_matrix(eigen_vectors[:, :num_components])
+    # Reconstructed dataset
+    X_reconstructed = (projection_matrix @ X_normalized.T).T
     return X_reconstruct 
 
 
-# How many principal components do we need in order to reach a mean squared error of less than 100 for the dataset?
- def mse(predict, actual):
-    '''Helper function that computes the mean squared error (MSE)'''
-    return np.square(predict - actual).sum(axis = 1).mean()
+ def mse(predicted, actual):
+    '''
+    Parameters:
+       predicted: ndarray of shape (N, D)
+       actual: ndarray of shape (N, D)
+
+    Returns: mse: float - mean squared error
+    
+    Computes the mean squared error (MSE) between predicted and actual data. In other
+    words: how many principal components are needed in order to reach a mean squared 
+    error of less than 100 for the dataset.
+    '''
+    # Calculate and return MSE
+    return np.square(predicted - actual).sum(axis = 1).mean()
 
 
-'''
-   PCA for high dimensional datasets
+def pca_high_dimensional(X, num_components):
+   '''
+   Parameters:
+   X: ndarray of size (N, D), where D is the dimension of the sample, N is 
+   the number of samples.
+   
+   num_components: int - number of principal components to use
+   
+   Returns:
+   X: ndarray of shape (N, D) the reconstruction of X from the first `num_components` 
+   pricipal components.
 
-   When the dimensionality of the dataset is larger than the number of given samples, 
-   PCA can be implemented in a more optimized way for high-dimensionality
+   Performs PCA for high-dimensional datasets where the number of features is larger 
+   than the number of samples (small sample size). When the dimensionality of the dataset 
+   is larger than the number of given samples, PCA can be implemented in a more optimized 
+   way for high-dimensionality.
+   '''
+   # Get dimensions of the dataset
+   N, D = X.shape
+   # Compute Gram matrix
+   gram_matrix = (X @ X.T) / N
+   # Compute eigenvalues and eigenvectors
+   eigen_values, eigen_vectors = compute_eigen(gram_matrix)
+   # Compute principle components
+   principal_components = (X.T @ eigen_vectors)[:, :num_components]
+   # Compute projection matrix
+   projection_matrix = compute_projection_matrix(principal_components)
+   # Reconstruct dataset
+   X_reconstructed = (projection_matrix @ X.T).T
+   return X_reconstruct 
 
-   Computes PCA for small a sample size but high-dimensional features
-   X = ndarray of size (N, D), where D is the dimension of the sample,
-   and N is the number of samples
-   num_components = the number of principal components to use
-   X_reconstruct = (N, D) ndarray
-   The reconstruction of X from the first `num_components` pricipal components
-'''
-def PCA_high_dim(X, n_components):
-    N, D = X.shape
-    M = (X @ X.T) / N
-    eig_vals, eig_vecs = eig(M)
-    U = (X.T @ eig_vecs)[:, :n_components]
-    P = projection_matrix(U)
-    X_reconstruct = (P @ X.T).T
-    return X_reconstruct 
 
+def time(func, repeat=10):
+   '''
+   Parameters:
+      func: function - function to measure time
+      repeat: int - number of repetitions
 
-# Defines a function that finds time/space complexity by comparing the running time between PCA and PCA_high_dim
-def time(f, repeat=10):
+   Return:
+   mean_time: float - mean execution time
+   std_time: float - standard deviation of execution time
+
+   Measure the execution time of a function. Finds time/space complexity by 
+   comparing the running time between pca and pca_high_dimensional functions.
+   '''
+   # Initialize list to store execution times
    times = []
+   # Repeat measurement
    for _ in range(repeat):
+      # Start timer
       start = timeit.default_timer()
-      f()
+      # Execute function
+      func()
+      # Stop timer
       stop = timeit.default_timer()
-      times.append(stop-start)
+      # Append elaspsed time to list
+      times.append(stop - start)
+   # Return mean and standard deviation
    return np.mean(times), np.std(times)
 
 
@@ -129,6 +211,9 @@ Xbar, mu, std = normalize(X)
 print(mu.shape)
 print(std.shape)
 
+# LEFT OFF HERE
+# LEFT OFF HERE
+# LEFT OFF HERE
 # The number of principal components we use, the smaller the reconstruction error will be
 for num_component in range(1, 20):
     from sklearn.decomposition import PCA as SKPCA
